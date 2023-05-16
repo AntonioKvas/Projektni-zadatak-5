e5bf2dc6db43006a2829f93641f8ff030febbc77
[Antonio <[akvas@tvz.hr]>
2023-05-16 18:16:33 +0200

    zad5

---
+++ korisnik/__init__.py
@@ -0,0 +1,4 @@
+from .unos_korisnika import unos_korisnika
+from .ispis_korisnika import get_korisnik, ispis_svih_korisnika
+from .korisnik import Korisnik
+from .osobnaiskaznica import OsobnaIskaznica
